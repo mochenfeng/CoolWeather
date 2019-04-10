073e7b5b245c2e7a1f91caa8eee1859d432c94d5
admin <1109525329@qq.com>
2019-04-10 09:05:48 +0800

    3

--- app/src/main/java/com/example/admin/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/admin/myapplication/Main2Activity.java
@@ -7,40 +7,34 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.Response;
 
+public class Main2Activity extends AppCompatActivity {
 
-import java.io.IOException;
-
-
-
-
-
-public class MainActivity extends AppCompatActivity {
-
-    private  TextView textView;
+    private TextView textView;
     private Button button;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        this.textView = (TextView) findViewById(R.id.abc);
-        this.button = (Button) findViewById(R.id.button);
+        setContentView(R.layout.activity_main2);
+        this.textView = (TextView) findViewById(R.id.text);
+        this.button = (Button) findViewById(R.id.btn);
         this.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity( new Intent(MainActivity.this, Main2Activity.class));
+                startActivity( new Intent(Main2Activity.this, MainActivity.class));
             }
         });
 
-        String weatherId="CN101320102";
-        String weatherUrl = "http://guolin.tech/api/weather?cityid=" + weatherId + "&key=40fc75aa40b44ef2923f42b345fbc2d6";
+//        String weatherId="CN101320102";
+        String weatherUrl = "http://guolin.tech/api/china";
         HttpUtil.sendOkHttpRequest(weatherUrl, new Callback(){
             @Override
-            public void  onResponse(Call call, Response response) throws IOException{
+            public void  onResponse(Call call, Response response) throws IOException {
                 final String responseText = response.body().string();
 //                textView.setText(responseText);
                 runOnUiThread(new Runnable() {
